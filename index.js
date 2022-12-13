a9fe4cfae8025da7f293bcb31b72f3da31e9570c
MBG0 <mehmetbgokcek@outlook.com>
2022-12-13 13:27:49 +0300

    new-version

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
 "use strict";
 
-// const firstBtn = document.querySelector(".btn-first");
-// const secondBtn = document.querySelector(".btn-second");
-
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-// var randomImageSource = "images/" + randomDiceImage;
-
-// var image1 = document.querySelectorAll("img")[0];
-// console.log(image1);
-// // image1.setAttribute("src", randomImageSource);
-
-// firstBtn.addEventListener("click", function () {
-//   image1.setAttribute("src", randomImageSource);
-// });
-
-// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-// if (randomNumber1 > randomNumber2) {
-//   document.querySelector("h1").innerHTML = "Player 1 Wins!";
-// } else if (randomNumber2 > randomNumber1) {
-//   document.querySelector("h1").innerHTML = "Player 2 Wins!";
-// } else if ((randomNumber1 = randomNumber2)) {
-//   document.querySelector("h1").innerHTML = "Draw!";
-// }
-
 const leftBtn = document.querySelector(".btn-first");
 const rightBtn = document.querySelector(".btn-second");
 
@@ -44,13 +13,9 @@ let dice = Math.trunc(Math.random() * 6) + 1;
 let dice2 = Math.trunc(Math.random() * 6) + 1;
 
 // * first and second dice
-console.log("dice: ", dice);
 let randomImage = "dice" + dice + ".png";
-console.log(randomImage);
 let randomImageSource = "images/" + randomImage;
-console.log(randomImageSource);
 let image1 = document.querySelectorAll("img")[0];
-console.log(image1);
 
 leftBtn.addEventListener("click", function () {
   dice = Math.trunc(Math.random() * 6) + 1;
